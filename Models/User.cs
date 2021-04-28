bd775860230a8a570801f74c519a3984dd24f140
hiinnn <hin_pansa@hotmail.com>
2021-04-29 00:13:34 +0700

    ADD: redesign user profile page

--- Models/User.cs
+++ Models/User.cs
@@ -4,20 +4,19 @@ using System.ComponentModel.DataAnnotations;
 
 namespace softstu_project.Models
 {
-    public enum Role
+    public enum User_role
     {
-        user, admin, super_admin
+        user, admin
     }
     public class User
     {
         [Key]
-        public int uuid { get; set; } 
+        public int uuid { get; set; }
         [StringLength(50, MinimumLength = 1)]
         [Required]
- 
-        public string role { get; set; }
+
+        public User_role role_id { get; set; }
         [Required]
-        [StringLength(20, MinimumLength = 8)]
 
         public string username { get; set; }
         [Required]
@@ -26,6 +25,16 @@ namespace softstu_project.Models
         public string password { get; set; }
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "0:{yyyy-MM-dd HH:mm:ss}", ApplyFormatInEditMode = true)]
+
+        public string fname { get; set; }
+
+        public string lname { get; set; }
+
+        public int student_id { get; set; }
+
+        public string faculty { get; set; }
+
+        public string department { get; set; }
 
         public string email { get; set; }
 
@@ -37,6 +46,21 @@ namespace softstu_project.Models
 
         public User()
         {
+        }
+
+        public User(User_role role_id, string username, string password, string fname, string lname, int student_id, string facaulty, string department, string email)
+        {
+            this.role_id = role_id;
+            this.username = username;
+            this.password = password;
+            this.fname = fname;
+            this.lname = lname;
+            this.student_id = student_id;
+            this.faculty = facaulty;
+            this.department = department;
+            this.email = email;
+            this.created = DateTime.Now;
+            this.updated = DateTime.Now;
         }
 
         /////////////////////////////////////////////////////////////////////////
